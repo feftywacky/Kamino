c724fac8a1f78ce62ca385e0df494f9a8f792a0d
feftywacky <linfeiyu14@gmail.com>
2024-02-05 10:56:42 -0500

    basic setup for document page

--- app/(main)/(routes)/documents/page.jsx
+++ app/(main)/(routes)/documents/page.jsx
@@ -1,9 +1,23 @@
 'use client';
 
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return (
-        <div>
-            <h1>Documents</h1>
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <h2 className="text-lg text-center font-medium">
+                Hello {user.firstName ? user.firstName : There}.
+                <br />
+                Your notes are ready.
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a new note
+            </Button>
         </div>
     );
 }
