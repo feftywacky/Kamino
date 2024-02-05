799b7c86334ef029027f3048b69a739e94ba8ac2
feftywacky <linfeiyu14@gmail.com>
2024-02-04 21:55:00 -0500

    init document page

---
+++ app/(main)/_components/navigation.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+
+export const Navigation = () => {
+    return (
+        <>
+            <aside
+                className="group/sidebar h-full bg-secondary overflow-y-auto realtive flex w-90 flex-col z-[999]"
+            >
+                <h1>Navigation</h1>
+            </aside>
+        </>
+        
+    );
+}
+
+export default Navigation;
+
+
