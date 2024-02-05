dda8fac6d83f4451c6c49e729a5b1eb201d65252
feftywacky <linfeiyu14@gmail.com>
2024-02-04 22:57:11 -0500

    allow sidebar to resize

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -3,7 +3,7 @@
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import { Spinner } from "@/components/ui/loading_animation";
-import { Navigation } from "./_components/navigation";
+import { Sidebar } from "./_components/sidebar";
 
 const MainLayout = ({ children }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -22,7 +22,7 @@ const MainLayout = ({ children }) => {
 
   return ( 
     <div className="h-full flex dark:bg-[#1F1F1F]">
-      <Navigation />
+      <Sidebar />
       <main className="flex-1 h-full overflow-y-auto">
         {children}
       </main>
