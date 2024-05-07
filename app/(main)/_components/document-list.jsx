9f3b08294e3ddebcd9188b04e25baa4e6721c1d4
feftywacky <linfeiyu14@gmail.com>
2024-05-07 00:14:27 -0400

    fix ui message bug when use has no pages

--- app/(main)/_components/document-list.jsx
+++ app/(main)/_components/document-list.jsx
@@ -8,20 +8,20 @@ import { cn } from "@/lib/utils";
 import { FileIcon } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 
-export const DocumentList = ({parentDocumentId, level=0, }) => {
-    
+export const DocumentList = ({ parentDocumentId, level = 0, }) => {
+
     const params = useParams();
     const router = useRouter();
     const [expanded, setExpanded] = useState({});
 
     const onExpand = (documentId) => {
-        setExpanded((prevExpanded)=>({
+        setExpanded((prevExpanded) => ({
             ...prevExpanded,
             [documentId]: !prevExpanded[documentId]
         }))
     }
 
-    const documents = useQuery(api.documents.getSidebar,{
+    const documents = useQuery(api.documents.getSidebar, {
         parentDocument: parentDocumentId
     })
 
@@ -29,42 +29,45 @@ export const DocumentList = ({parentDocumentId, level=0, }) => {
         router.push(`/documents/${documentId}`)
     }
 
-    if (documents===undefined){
+    if (documents === undefined) {
         return (
             <>
-                <Item.Skeleton level={level}/>
-                {level===0 && (
+                <Item.Skeleton level={level} />
+                {level === 0 && (
                     <>
-                        <Item.Skeleton level={level}/>
-                        <Item.Skeleton level={level}/>
+                        <Item.Skeleton level={level} />
+                        <Item.Skeleton level={level} />
                     </>
                 )}
             </>
         )
     }
 
+    const paddingLeftStyle = level ? `${(level * 12) + 25}px` : undefined;
+    const baseClassName =  "hidden text-sm font-medium text-muted-foreground/80 ml-4";
+    const conditionalClassName = `${expanded && "last:block"} ${level===0 && "hidden"}`;
+    const message = documents.length === 0 ? "You have no pages" : "No pages inside";
+
     return (
         <>
-            <p
-                style={{paddingLeft: level? `${(level*12)+25}px` : undefined}}
-                className={cn("hidden text-sm font-medium text-muted-foreground/80 ml-2", expanded && "last:block", level===0 && "hidden")}
-            >
-                No Pages Inside
+            <p style={{ paddingLeft: paddingLeftStyle }} className={cn(`${baseClassName} ${conditionalClassName}`)}>
+                {message}
             </p>
-            {documents.map((document)=>(
+
+            {documents.map((document) => (
                 <div key={document._id}>
                     <Item
                         id={document._id}
-                        onClick={()=>onRedirect(document._id)}
+                        onClick={() => onRedirect(document._id)}
                         label={document.title}
                         icon={FileIcon}
                         documentIcon={document.icon}
-                        active={params.documentId===document._id}
+                        active={params.documentId === document._id}
                         level={level}
-                        onExpand={()=>onExpand(document._id)}
+                        onExpand={() => onExpand(document._id)}
                         expanded={expanded[document._id]}
                     />
-                        {expanded[document._id] && (<DocumentList parentDocumentId={document._id} level={level+1}/>)}
+                    {expanded[document._id] && (<DocumentList parentDocumentId={document._id} level={level + 1} />)}
                 </div>
             ))}
         </>
